f9fc9a671909db254e65280e6bbf0d9c1870ec51
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-10-08 13:41:19 +0200

    :pencil2: change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OntoAligner",
-    version="0.1.0",
+    version="0.1.1",
     author="Hamed Babaei Giglou",
     author_email="hamedbabaeigiglou@gmail.com",
     description="OntoAligner: A Ontology Alignment Python Library",
