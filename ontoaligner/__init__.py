5745b67756e9463b97a0bf95c6fa84550108f5e8
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-10-08 11:11:12 +0200

    :pencil2: project __init__

---
+++ ontoaligner/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.1.0"
+
+# import logging
+#
+# from onmatcher import base, encoder, evaluation, ontology, ontology_matchers, utils, postprocess
+# from onmatcher.pipeline import OMPipelines
+#
+# __all__ = [
+#     "base",
+#     "ontology",
+#     "utils",
+#     "ontology_matchers",
+#     "encoder",
+#     "pipeline",
+#     "OMPipelines",
+#     "evaluation",
+#     "postprocess"
+# ]
+#
+# # Root logger configuration
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+#
+# stdout = logging.StreamHandler()
+# stdout.setLevel(logging.DEBUG)
+#
+# formatter = logging.Formatter("%(name)s - %(levelname)s: %(message)s")
+# stdout.setFormatter(formatter)
+#
+# logger.addHandler(stdout)
