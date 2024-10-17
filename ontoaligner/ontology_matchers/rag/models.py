fe15206e95e2f23a2ab6fc5fe498b854d82588c5
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-10-17 23:48:56 +0200

    :recycle: refactor code to rename models & path

--- ontoaligner/ontology_matchers/rag/models.py
+++ ontoaligner/ontology_matchers/rag/models.py
@@ -1,237 +1,116 @@
 # -*- coding: utf-8 -*-
-
-from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer,
-    LlamaForCausalLM,
-    LlamaTokenizer,
-    MistralForCausalLM,
-)
-
-from .rag import RAG, RAGBasedDecoderLLMArch, RAGBasedOpenAILLMArch
+from .rag import RAG, LLaMADecoderRAGLLM, MistralDecoderRAGLLM, OpenAIRAGLLM, AutoModelDecoderRAGLLMV2, MambaSSMRAGLLM, AutoModelDecoderRAGLLM
 from ..retrieval.models import AdaRetrieval, BERTRetrieval
 
-from typing import Any
-import torch
 
-
-class LLaMA7BDecoderLM(RAGBasedDecoderLLMArch):
-    tokenizer = LlamaTokenizer
-    model = LlamaForCausalLM
-    path = "meta-llama/Llama-2-7b-hf"
-
-    def __str__(self):
-        return super().__str__() + "-LLaMA-2-7B"
-
-
-class Mistral7BDecoderLM(RAGBasedDecoderLLMArch):
-    tokenizer = AutoTokenizer
-    model = MistralForCausalLM
-    path = "mistralai/Mistral-7B-v0.1"
-
-    def __str__(self):
-        return super().__str__() + "-MistralLM-7B-v0.1"
-
-
-class Falcon7BDecoderLM(RAGBasedDecoderLLMArch):
-    tokenizer = AutoTokenizer
-    model = AutoModelForCausalLM
-    path = "tiiuae/falcon-7b"
-
-    def __str__(self):
-        return super().__str__() + "-falcon-7b"
-
-    def get_probas_yes_no(self, outputs):
-        probas_yes_no = (
-            outputs.scores[0][:, self.answer_sets_token_id["yes"] + self.answer_sets_token_id["no"]]
-            .float()
-            .softmax(-1)
-        )
-        return probas_yes_no
-
-    def check_answer_set_tokenizer(self, answer: str) -> bool:
-        return len(self.tokenizer(answer).input_ids) == 1
-
-
-class Vicuna7BDecoderLM(RAGBasedDecoderLLMArch):
-    tokenizer = AutoTokenizer
-    model = AutoModelForCausalLM
-    path = "lmsys/vicuna-7b-v1.5"
-
-    def __str__(self):
-        return super().__str__() + "-vicuna-7b-v1.5"
-
-
-class MPT7BDecoderLM(RAGBasedDecoderLLMArch):
-    tokenizer = AutoTokenizer
-    model = AutoModelForCausalLM
-    path = "mosaicml/mpt-7b"
-
-    def __str__(self):
-        return super().__str__() + "-mpt-7b"
-
-    def get_probas_yes_no(self, outputs):
-        probas_yes_no = (
-            outputs.scores[0][:, self.answer_sets_token_id["yes"] + self.answer_sets_token_id["no"]]
-            .float()
-            .softmax(-1)
-        )
-        return probas_yes_no
-
-    def check_answer_set_tokenizer(self, answer: str) -> bool:
-        return len(self.tokenizer(answer).input_ids) == 1
-
-
-class ChatGPTOpenAILLM(RAGBasedOpenAILLMArch):
-    path = "gpt-3.5-turbo-1106"
-
-    def __str__(self):
-        return super().__str__() + "-GPT-3.5"
-
-
-class LLaMA7BLLMAdaRAG(RAG):
+class LLaMALLMAdaRetriever(RAG):
     Retrieval = AdaRetrieval
-    LLM = LLaMA7BDecoderLM
+    LLM = LLaMADecoderRAGLLM
 
     def __str__(self):
-        return super().__str__() + "-LLaMA7BAdaRAG"
+        return super().__str__() + "-LLaMALLMAdaRetriever"
 
-
-class MistralLLMAdaRAG(RAG):
-    Retrieval = AdaRetrieval
-    LLM = Mistral7BDecoderLM
-
-    def __str__(self):
-        return super().__str__() + "-MistralLMAdaRAG"
-
-
-class LLaMA7BLLMBertRAG(RAG):
+class LLaMALLMBERTRetriever(RAG):
     Retrieval = BERTRetrieval
-    LLM = LLaMA7BDecoderLM
+    LLM = LLaMADecoderRAGLLM
 
     def __str__(self):
-        return super().__str__() + "-LLaMA7BLLMBertRAG"
+        return super().__str__() + "-LLaMALLMBERTRetriever"
 
 
-class MistralLLMBertRAG(RAG):
-    Retrieval = BERTRetrieval
-    LLM = Mistral7BDecoderLM
-
-    def __str__(self):
-        return super().__str__() + "-MistralLLMBertRAG"
-
-
-class ChatGPTOpenAIAdaRAG(RAG):
+class MistralLLMAdaRetriever(RAG):
     Retrieval = AdaRetrieval
-    LLM = ChatGPTOpenAILLM
+    LLM = MistralDecoderRAGLLM
 
     def __str__(self):
-        return super().__str__() + "-ChatGPTOpenAIAdaRAG"
+        return super().__str__() + "-MistralLLMAdaRetriever"
 
 
-class FalconLLMAdaRAG(RAG):
-    Retrieval = AdaRetrieval
-    LLM = Falcon7BDecoderLM
-
-    def __str__(self):
-        return super().__str__() + "-FalconLLMAdaRAG"
-
-
-class FalconLLMBertRAG(RAG):
+class MistralLLMBERTRetriever(RAG):
     Retrieval = BERTRetrieval
-    LLM = Falcon7BDecoderLM
+    LLM = MistralDecoderRAGLLM
 
     def __str__(self):
-        return super().__str__() + "-FalconLLMBertaRAG"
+        return super().__str__() + "-MistralLLMBERTRetriever"
 
 
-class VicunaLLMAdaRAG(RAG):
+class GPTOpenAILLMAdaRetriever(RAG):
     Retrieval = AdaRetrieval
-    LLM = Vicuna7BDecoderLM
+    LLM = OpenAIRAGLLM
 
     def __str__(self):
-        return super().__str__() + "-VicunaLLMAdaRAG"
+        return super().__str__() + "-GPTOpenAILLMAdaRetriever"
 
-
-class VicunaLLMBertRAG(RAG):
+class GPTOpenAILLMBERTRetriever(RAG):
     Retrieval = BERTRetrieval
-    LLM = Vicuna7BDecoderLM
+    LLM = OpenAIRAGLLM
 
     def __str__(self):
-        return super().__str__() + "-VicunaLLMBertRAG"
+        return super().__str__() + "-GPTOpenAILLMBERTRetriever"
 
 
-class MPTLLMAdaRAG(RAG):
+class FalconLLMAdaRetriever(RAG):
     Retrieval = AdaRetrieval
-    LLM = MPT7BDecoderLM
+    LLM = AutoModelDecoderRAGLLMV2
 
     def __str__(self):
-        return super().__str__() + "-MPTLLMAdaRAG"
+        return super().__str__() + "-FalconLLMAdaRetriever"
 
 
-class MPTLLMBertRAG(RAG):
+class FalconLLMBERTRetriever(RAG):
     Retrieval = BERTRetrieval
-    LLM = MPT7BDecoderLM
+    LLM = AutoModelDecoderRAGLLMV2
 
     def __str__(self):
-        return super().__str__() + "-MPTLLMBertRAG"
+        return super().__str__() + "-FalconLLMBERTRetriever"
 
 
-class Mamba3BSSMLLM(RAGBasedDecoderLLMArch):
-    tokenizer = AutoTokenizer
-    model = AutoModelForCausalLM
-    path = "Q-bert/Mamba-3B"
-
-    def __str__(self):
-        return super().__str__() + "-mamba-3B"
-
-    def load_model(self) -> None:
-        if self.kwargs["device"] != "cpu":
-            self.model = self.model.from_pretrained(
-                self.path,
-                load_in_8bit=True,
-                device_map="balanced",
-                trust_remote_code=True,
-            )
-        else:
-            self.model = self.model.from_pretrained(self.path, trust_remote_code=True)
-            self.model.to(self.kwargs["device"])
-
-    def generate_for_llm(self, tokenized_input_data: Any) -> Any:
-        with torch.cuda.amp.autocast():
-            outputs = self.model.generate(
-                tokenized_input_data.input_ids,
-                pad_token_id=self.tokenizer.eos_token_id,
-                max_new_tokens=self.kwargs["max_token_length"],
-                do_sample=False,
-                output_scores=True,
-                return_dict_in_generate=True
-            )
-        return outputs
-
-    def get_probas_yes_no(self, outputs):
-        probas_yes_no = (
-            outputs.scores[0][:, self.answer_sets_token_id["yes"] + self.answer_sets_token_id["no"]]
-            .float()
-            .softmax(-1)
-        )
-        return probas_yes_no
-
-    def check_answer_set_tokenizer(self, answer: str) -> bool:
-        return len(self.tokenizer(answer).input_ids) == 1
 
 
-class MambaLLMAdaRAG(RAG):
+
+class VicunaLLMAdaRetriever(RAG):
     Retrieval = AdaRetrieval
-    LLM = Mamba3BSSMLLM
+    LLM = AutoModelDecoderRAGLLM
 
     def __str__(self):
-        return super().__str__() + "-MambaLLMAdaRAG"
+        return super().__str__() + "-VicunaLLMAdaRetriever"
 
 
-class MambaLLMBertRAG(RAG):
+class VicunaLLMBERTRetriever(RAG):
     Retrieval = BERTRetrieval
-    LLM = Mamba3BSSMLLM
+    LLM = AutoModelDecoderRAGLLM
 
     def __str__(self):
-        return super().__str__() + "-MambaLLMBertRAG"
+        return super().__str__() + "-VicunaLLMBERTRetriever"
+
+
+class MPTLLMAdaRetriever(RAG):
+    Retrieval = AdaRetrieval
+    LLM = AutoModelDecoderRAGLLMV2
+
+    def __str__(self):
+        return super().__str__() + "-MPTLLMAdaRetriever"
+
+
+class MPTLLMBERTRetriever(RAG):
+    Retrieval = BERTRetrieval
+    LLM = AutoModelDecoderRAGLLMV2
+
+    def __str__(self):
+        return super().__str__() + "-MPTLLMBERTRetriever"
+
+
+class MambaLLMAdaRetriever(RAG):
+    Retrieval = AdaRetrieval
+    LLM = MambaSSMRAGLLM
+
+    def __str__(self):
+        return super().__str__() + "-MambaLLMAdaRetriever"
+
+
+class MambaLLMBERTRetriever(RAG):
+    Retrieval = BERTRetrieval
+    LLM = MambaSSMRAGLLM
+
+    def __str__(self):
+        return super().__str__() + "-MambaLLMBERTRetriever"
