fb3596b9d5575d092c411b498b8b8922f972fe15
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-11-08 18:09:14 +0100

    :pencil2: fix __str__

--- ontoaligner/ontology_matchers/lightweight/models.py
+++ ontoaligner/ontology_matchers/lightweight/models.py
@@ -8,7 +8,7 @@ from .lightweight import FuzzySMLightweight
 
 class SimpleFuzzySMLightweight(FuzzySMLightweight):
     def __str__(self):
-        return super().__str__() + "+SimpleFuzzySM"
+        return super().__str__() + "-SimpleFuzzySMLightweight"
 
     def ratio_estimate(self) -> Any:
         return rapidfuzz.fuzz.ratio
@@ -16,7 +16,7 @@ class SimpleFuzzySMLightweight(FuzzySMLightweight):
 
 class WeightedFuzzySMLightweight(FuzzySMLightweight):
     def __str__(self):
-        return super().__str__() + "+WeightedFuzzySM"
+        return super().__str__() + "-WeightedFuzzySMLightweight"
 
     def ratio_estimate(self) -> Any:
         return rapidfuzz.fuzz.WRatio
@@ -24,7 +24,7 @@ class WeightedFuzzySMLightweight(FuzzySMLightweight):
 
 class TokenSetFuzzySMLightweight(FuzzySMLightweight):
     def __str__(self):
-        return super().__str__() + "+TokenSetFuzzySM"
+        return super().__str__() + "-TokenSetFuzzySMLightweight"
 
     def ratio_estimate(self) -> Any:
         return rapidfuzz.fuzz.token_set_ratio
