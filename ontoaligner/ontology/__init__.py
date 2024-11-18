708502c66e82469b61223255b18add76c77d5261
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-11-19 00:15:18 +0100

    :memo: added root imports

--- ontoaligner/ontology/__init__.py
+++ ontoaligner/ontology/__init__.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+from .anatomy import *
+from .biodiv import *
+from .bioml import *
+from .commonkg import *
+from .food import *
+from .mse import *
+from .phenotype import *
