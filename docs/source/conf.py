c92b04281969da8213fa098c3b91677db1889d63
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-11-28 18:41:59 +0100

    :bug: sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_press_theme"
+html_theme = "press"
 
 html_theme_options = {
     'repository_url': 'https://github.com/sciknoworg/OntoAligner',
