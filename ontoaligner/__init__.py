ab734370c3049cbcfefc000a99c10093ebba2072
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-12-06 00:11:38 +0100

    :memo: versioning

--- ontoaligner/__init__.py
+++ ontoaligner/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 from .pipeline import Pipeline
--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10",
+    python_requires=">=3.10,<4.0.0",
     project_urls={
         "Documentation": "https://ontoaligner.readthedocs.io/",
         "Source": "https://github.com/sciknoworg/OntoAligner",
