0753239b49812c955a67b7bec5839a6a6419f8ac
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-12-05 23:18:46 +0100

    :pencil2: refactor

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="OntoAligner",
-    version="0.1.1",
+    version="1.0.0",
     author="Hamed Babaei Giglou",
     author_email="hamedbabaeigiglou@gmail.com",
-    description="OntoAligner: A Ontology Alignment Python Library",
-    long_description=open('README.md').read(),
+    description="OntoAligner: A Comprehensive Modular and Robust Python Toolkit for Ontology Alignment",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/HamedBabaei/OntoAligner",
+    url="https://github.com/sciknoworg/OntoAligner",
     packages=find_packages(),
     install_requires=[
         "pathlib",
@@ -16,23 +19,32 @@ setup(
         "datasets",
         "numpy",
         "pandas",
-        "scikit_learn",
+        "scikit-learn",
         "ontospy==2.1.1",
         "openai==1.56.0",
         "owlready2==0.44",
         "rank_bm25==0.2.2",
         "rapidfuzz==3.5.2",
         "rdflib==7.1.1",
-        "sentence_transformers==2.2.2",
+        "sentence-transformers==2.2.2",
         "setfit==1.0.3",
         "torch==2.5.0",
         "tqdm==4.66.3",
         "transformers==4.46.0"
     ],
-    classifiers=[  # Optional metadata
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires=">=3.10",
+    project_urls={
+        "Documentation": "https://ontoaligner.readthedocs.io/",
+        "Source": "https://github.com/sciknoworg/OntoAligner",
+        "Tracker": "https://github.com/sciknoworg/OntoAligner/issues",
+    },
 )
