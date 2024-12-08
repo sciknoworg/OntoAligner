356c5be7361760ba76ae282f027b341461d42c49
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-12-08 14:17:48 +0100

    :memo: version 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="OntoAligner",
-    version="1.0.0",
+    version="1.1.0",
     author="Hamed Babaei Giglou",
     author_email="hamedbabaeigiglou@gmail.com",
     description="OntoAligner: A Comprehensive Modular and Robust Python Toolkit for Ontology Alignment",
