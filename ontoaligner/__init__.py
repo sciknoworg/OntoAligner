f049f4b07bab881a8b09cac438e2c894618922b4
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-12-09 22:50:55 +0100

    :pencil2: add __all__ var for the lib

--- ontoaligner/__init__.py
+++ ontoaligner/__init__.py
@@ -2,3 +2,14 @@
 __version__ = "1.1.0"
 
 from .pipeline import OntoAlignerPipeline
+from ontoaligner import ontology, base, encoder, ontology_matchers, utils, postprocess
+
+__all__ = [
+    "ontology",
+    "base",
+    "encoder",
+    "ontology_matchers",
+    "utils",
+    "postprocess",
+    "OntoAlignerPipeline"
+]
