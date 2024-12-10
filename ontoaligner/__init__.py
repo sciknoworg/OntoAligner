40409c5d562eb42cf614797028d2b5dfa159cfcb
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-12-10 01:01:31 +0100

    :sparkles: add v1.2.0 versioning

--- ontoaligner/__init__.py
+++ ontoaligner/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 from .pipeline import OntoAlignerPipeline
 from ontoaligner import ontology, base, encoder, ontology_matchers, utils, postprocess
