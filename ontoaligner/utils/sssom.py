fd8fac4df019fc97c92becb85fc3dbfd01ba501c
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-08-26 15:04:00 +0200

    Update sssom.py

--- ontoaligner/utils/sssom.py
+++ ontoaligner/utils/sssom.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import datetime
 from pathlib import Path
 from typing import Any, TextIO
 
@@ -12,6 +13,7 @@ from curies import vocabulary as v
 from sssom_pydantic import MappingSet, SemanticMapping
 
 __all__ = [
+    "to_semantic_mappings",
     "write_sssom",
 ]
 
@@ -24,6 +26,15 @@ def write_sssom(
     metadata: MappingSet | None = None,
     **kwargs: Any,
 ) -> None:
+    """Convert matchings to SSSOM objects and write a TSV to disk.
+
+    :param matchings: OntoAligner matchings
+    :param path: The path or file to write to.
+    :param converter:
+    :param metadata:
+    :param kwargs:
+    :return:
+    """
     if converter is None:
         converter = bioregistry.get_default_converter()
     mappings = to_semantic_mappings(matchings, converter)
@@ -38,7 +49,8 @@ LOOKUPS = {
 
 
 def to_semantic_mappings(
-    matchings: list[dict], converter: curies.Converter | None = None,
+    matchings: list[dict],
+    converter: curies.Converter | None = None,
 ) -> list[SemanticMapping]:
     if converter is None:
         converter = bioregistry.get_default_converter()
@@ -46,7 +58,7 @@ def to_semantic_mappings(
     for matching in matchings:
         try:
             mapping = _to_semantic_mapping(matching, converter)
-        except Exception:
+        except Exception:  # noqa: BLE001
             continue
         else:
             if mapping is not None:
@@ -65,6 +77,12 @@ def _to_semantic_mapping(
         subject=sub,
         predicate=LOOKUPS[matching.get("relation", "=")],
         object=obj,
+        # depending on the aligner, this might be one of several
+        # other things from the SEMAPV namespace
         justification=v.unspecified_matching_process,
         confidence=matching.get("score"),
+        mapping_date=datetime.date.today(),  # noqa: DTZ011
+        # TODO in follow-up, add additional fields such as:
+        #  mapping tool, license, etc. from
+        #  https://mapping-commons.github.io/sssom/dev/
     )
